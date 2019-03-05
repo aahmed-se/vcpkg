bbe955dedca5fc34550442085e97fe1726fc163e
Ali Ahmed <alahmed.se@gmail.com>
2019-03-05 13:53:51 -0800

    Add Pulsar 2.3.0 draft

---
+++ ports/libpulsar/portfile.cmake
@@ -0,0 +1,22 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO apache/pulsar
+    REF v2.3.0
+    SHA512 7b019eede6dd45bef98e2fff7bf775ace8c8dc9a88effb50ea3ff80a1c7f0955c650806a8b98a3947fadce8362455daa93b2c20d8fd454778c2b3b58c03fff55
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/pulsar-client-cpp
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_TESTS=OFF 
+        -DBUILD_PYTHON_WRAPPER=OFF
+        -DLINK_STATIC=OFF
+        -DPULSAR_LIBRARY_NAME=pular
+        -DPULSAR_LIBRARY_VERSION=2.3.0
+)
+
+vcpkg_install_cmake()
