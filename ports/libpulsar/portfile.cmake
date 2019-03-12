94a9082fa287f2b8aea9517942a3d7e431b93d23
Ali Ahmed <alahmed.se@gmail.com>
2019-03-11 17:04:51 -0700

    Update

--- ports/libpulsar/portfile.cmake
+++ ports/libpulsar/portfile.cmake
@@ -2,21 +2,21 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO apache/pulsar
-    REF v2.3.0
-    SHA512 7b019eede6dd45bef98e2fff7bf775ace8c8dc9a88effb50ea3ff80a1c7f0955c650806a8b98a3947fadce8362455daa93b2c20d8fd454778c2b3b58c03fff55
-    HEAD_REF master
+    REPO aahmed-se/incubator-pulsar
+    REF 076d6f7dbec6c4c0f4aa4d74beaa5c9c8025e45a
+    SHA512 64aea7f8d2d215d1665c41b74f77c4a2e97150fa82c0fdc671c7ebbe69217a1bb48b1d5b9820e342a7ace7daa02d40db891963f8a78d7a0817d7a5c50a3d053e
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}/pulsar-client-cpp
-    PREFER_NINJA
     OPTIONS
         -DBUILD_TESTS=OFF 
         -DBUILD_PYTHON_WRAPPER=OFF
-        -DLINK_STATIC=OFF
-        -DPULSAR_LIBRARY_NAME=pular
-        -DPULSAR_LIBRARY_VERSION=2.3.0
+        # -DLINK_STATIC=ON
+        -DLIB_NAME=pular
+        -DLIBRARY_VERSION=2.3.0
 )
 
+
+#vcpkg_build_cmake()
 vcpkg_install_cmake()
